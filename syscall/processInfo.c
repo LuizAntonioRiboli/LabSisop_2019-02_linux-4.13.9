7ebcd440877f63a7bb8b59c793c9c36c87d3bf8c
Laboratório de Redes <labredes@Labredes.pucrs.br>
2019-09-17 08:13:33 -0300

    process info

--- syscall/processInfo.c
+++ syscall/processInfo.c
@@ -10,30 +10,55 @@ unsigned char kbuf[256];
 int bufsz;
 int ret;
 
-/* Find the process */
-for_each_process(proces) {
-	if( (long)task_pid_nr(proces) == pid){
-		/* Print the process info to the buffer */
-		snprintf(kbuf, sizeof(kbuf), "Process: %s\n PID_Number: %ld\n Process State: %ld\n Priority: %ld\n RT_Priority: %ld\n Static Priority: %ld\n Normal Priority: %ld\n", 
-				proces->comm, 
-				(long)task_pid_nr(proces), 
-				(long)proces->state, 
-				(long)proces->prio, 
-				(long)proces->rt_priority, 
-				(long)proces->static_prio, (long)proces->normal_prio);
-		bufsz = strlen(kbuf)+1;
+int index = 0;
 
-		/* User buffer is too small */
-		if(bufsz > size){
-			return -1;
+	/* Find the process */
+	for_each_process(proces) {
+		if( (long)task_pid_nr(proces) == pid){
+			/* Print the process info to the buffer */
+			snprintf(kbuf, sizeof(kbuf), "Process: %s\n PID_Number: %ld\n Process State: %ld\n Priority: %ld\n RT_Priority: %ld\n Static Priority: %ld\n Normal Priority: %ld\n", 
+					proces->comm, 
+					(long)task_pid_nr(proces), 
+					(long)proces->state, 
+					(long)proces->prio, 
+					(long)proces->rt_priority, 
+					(long)proces->static_prio, (long)proces->normal_prio);
+			bufsz = strlen(kbuf)+1;
+
+			/* User buffer is too small */
+			if(bufsz > size){
+				return -1;
+			}
+
+			/* success */
+			//ret = copy_to_user((void*)buf, (void*)kbuf, bufsz);
+
+			//return bufsz - ret;
+		}
+	}
+
+	// desafio 1
+	for_each_process(proces) {
+		if ( (long)proces->state != TASK_RUNNING) {
+			kbuf[index] = (long)task_pid_nr(proces);
+			index++;
+
+
 		}
 
-		/* success */
-		ret = copy_to_user((void*)buf, (void*)kbuf, bufsz);
 
-		return bufsz - ret;
 	}
-}
+
+	// quantidade de posicoes que coloquei vezes long
+	bufsz = index * sizeof(long);
+
+	/* success */
+	ret = copy_to_user((void*)buf, (void*)kbuf, bufsz);
+
+	//return bufsz - ret; 
+
+	// retorna quantas posicoes foram preenchidas
+	return index;	// desafio 1
 
 /* Process not found */
 return -2;	
